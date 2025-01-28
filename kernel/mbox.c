0e5b69dc1014a1fa8b486499c6b561dc21e352a3
Hao Gu <haogu2003@gmail.com>
2025-01-28 06:10:13 +0000

    quest 6 accopmlished

--- kernel/mbox.c
+++ kernel/mbox.c
@@ -277,15 +277,20 @@ static int do_fb_init(struct fb_struct *fbs) {
         // quest: OS logo
         // save framebuf ptr to fbs->fb
         /* STUDENT_TODO: your code here */
+        // It’s a 30-bit physical address after &=0x3FFFFFFF above.
+        fbs->fb = (unsigned char *)(unsigned long)(mbox[28]);
         fbs->width=mbox[5];
         // save height 
         /* STUDENT_TODO: your code here */
+        fbs->height=mbox[6];
         fbs->vwidth=mbox[10];
         fbs->vheight=mbox[11];        
         fbs->depth=mbox[20]; 
         fbs->isrgb=mbox[24];     // channel order        
         // save pitch
         /* STUDENT_TODO: your code here */
+        fbs->pitch=mbox[33];
+
         if(fbs->pitch * fbs->vheight > mbox[29])  // possible that pitch*vheight < actual allocation
             {W("pitch %d x vheight %d!= mbox[29] %u", fbs->pitch, fbs->vheight, mbox[29]);BUG();}
         fbs->size = PGROUNDUP(fbs->pitch * fbs->vheight);  // roundup b/c we'll reserve pages for it
@@ -427,7 +432,11 @@ void fb_showpicture()
     
     // quest: OS logo
     for(y=0;y<img_fb_height;y++) {
+        unsigned char *rowptr = ptr;
         for(x=0;x<img_fb_width;x++) {
+
+            memset(pixel, 0, 4); //to clear maybe uninitialized warning
+            
             HEADER_PIXEL(data, pixel);
             /* the image is in RGB. So if we have an RGB framebuffer, we copy
             the pixels directly, but for BGR we must swap R (pixel[0]) and B
@@ -435,15 +444,33 @@ void fb_showpicture()
             // extract r,g,b from "pixel", then assign that to *ptr
             // if you color does not look right, check "isrgb" in the_fb
             /* STUDENT_TODO: your code here */
+            // If the_fb.isrgb == 1 => store as (R, G, B).
+            // If the_fb.isrgb == 0 => swap R & B => store as (B, G, R).
+            if (the_fb.isrgb == 1) {
+                rowptr[x*4 + 0] = pixel[0];  // R
+                rowptr[x*4 + 1] = pixel[1];  // G
+                rowptr[x*4 + 2] = pixel[2];  // B
+            } else {
+                rowptr[x*4 + 0] = pixel[2];  // B
+                rowptr[x*4 + 1] = pixel[1];  // G
+                rowptr[x*4 + 2] = pixel[0];  // R
+            }
+            // alpha channel = opaque
+            rowptr[x*4 + 3] = 0xFF;  // A
         }
+        
         // advance ptr to the start of the next line of the pixels
         /* STUDENT_TODO: your code here */
+        ptr += the_fb.pitch;
     }
 
     // show text strings
     // quest: OS logo. 
     // adjust x/y so that the text starts from right below the picture     
     /* STUDENT_TODO: your code here */
+    x = (the_fb.vwidth - 200) / 2; // some rough horizontal center
+    y = ((the_fb.vheight - img_fb_height) / 2) + img_fb_height + 10; // below
+
     fb_print(&x, &y, "UVA OS");    
     sprintf(res, " %dx%d", the_fb.width, the_fb.height); // debug info 
     fb_print(&x, &y, res);
